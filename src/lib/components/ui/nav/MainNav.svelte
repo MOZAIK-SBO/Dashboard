ee543d8fd906fb30f95def4d92ab70a1bc05f942
mvkenhov <michiel.vankenhove@ugent.be>
2024-03-09 22:29:23 +0100

    refactor: abstract navigation links in own component

--- src/lib/components/ui/nav/MainNav.svelte
+++ src/lib/components/ui/nav/MainNav.svelte
@@ -1,9 +1,8 @@
 <script>
 	import { base } from '$app/paths';
-	import { page } from '$app/stores';
 	import { getUserClientStore } from '$lib/stores/UserClientStore';
-	import * as Tooltip from '$lib/components/ui/tooltip';
 	import LightSwitch from '../LightSwitch.svelte';
+	import NavLink from './NavLink.svelte';
 
 	const userClientStore = getUserClientStore();
 	$: userClientAvailable = $userClientStore.client_jwt_token != null;
@@ -11,50 +10,13 @@
 
 <nav class="mx-6 w-full flex justify-between">
 	<div class="flex items-center space-x-4 lg:space-x-6">
-		<a
-			href="{base}/"
-			class="text-sm font-medium transition-colors hover:text-primary"
-			class:text-muted-foreground={$page.url.pathname !== `${base}/`}>Home</a
-		>
+		<NavLink linkName="Home" href="{base}/" disabled={false} />
 
-		<Tooltip.Root openDelay={0}>
-			<Tooltip.Trigger>
-				<a
-					href={userClientAvailable ? `${base}/data` : null}
-					class="text-sm font-medium transition-colors"
-					class:hover:text-primary={userClientAvailable}
-					class:cursor-not-allowed={!userClientAvailable}
-					class:text-muted-foreground={$page.url.pathname !== `${base}/data`}>Data</a
-				>
-			</Tooltip.Trigger>
-			<Tooltip.Content>Save client settings first!</Tooltip.Content>
-		</Tooltip.Root>
+		<NavLink linkName="Data" href="{base}/data" disabled={!userClientAvailable} />
 
-		<Tooltip.Root openDelay={0}>
-			<Tooltip.Trigger>
-				<a
-					href={userClientAvailable ? `${base}/analysis` : null}
-					class="text-sm font-medium transition-colors"
-					class:hover:text-primary={userClientAvailable}
-					class:cursor-not-allowed={!userClientAvailable}
-					class:text-muted-foreground={$page.url.pathname !== `${base}/analysis`}>Analysis</a
-				>
-			</Tooltip.Trigger>
-			<Tooltip.Content>Save client settings first!</Tooltip.Content>
-		</Tooltip.Root>
+		<NavLink linkName="Analysis" href="{base}/analysis" disabled={!userClientAvailable} />
 
-		<Tooltip.Root openDelay={0}>
-			<Tooltip.Trigger>
-				<a
-					href={userClientAvailable ? `${base}/results` : null}
-					class="text-sm font-medium transition-colors"
-					class:hover:text-primary={userClientAvailable}
-					class:cursor-not-allowed={!userClientAvailable}
-					class:text-muted-foreground={$page.url.pathname !== `${base}/results`}>Results</a
-				>
-			</Tooltip.Trigger>
-			<Tooltip.Content>Save client settings first!</Tooltip.Content>
-		</Tooltip.Root>
+		<NavLink linkName="Results" href="{base}/results" disabled={!userClientAvailable} />
 	</div>
 	<div class="flex items-center space-x-4">
 		<a
---
+++ src/lib/components/ui/nav/NavLink.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import * as Tooltip from '$lib/components/ui/tooltip';
+
+	export let linkName: string;
+	export let href: string;
+	export let disabled = false;
+</script>
+
+{#if disabled}
+	<Tooltip.Root openDelay={0} closeOnEscape={false} closeOnPointerDown={false} group="nav-tooltip">
+		<Tooltip.Trigger>
+			<a
+				href={null}
+				class="text-sm font-medium transition-colors cursor-not-allowed text-muted-foreground"
+				>{linkName}</a
+			>
+		</Tooltip.Trigger>
+		<Tooltip.Content>Save client settings first!</Tooltip.Content>
+	</Tooltip.Root>
+{:else}
+	<a
+		{href}
+		class="text-sm font-medium transition-colors hover:text-primary"
+		class:text-muted-foreground={$page.url.pathname !== href}>{linkName}</a
+	>
+{/if}
