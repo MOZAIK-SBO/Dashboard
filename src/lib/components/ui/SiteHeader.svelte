463f2e716f8ea7466a6af9bac5db369e791e5f17
mvkenhov <michiel.vankenhove@ugent.be>
2024-03-11 20:52:38 +0100

    fix: inherit background color

--- src/lib/components/ui/SiteHeader.svelte
+++ src/lib/components/ui/SiteHeader.svelte
@@ -2,7 +2,7 @@
 	import MainNav from './nav/MainNav.svelte';
 </script>
 
-<header class="sticky top-0 z-50 w-full border-b border-border/70">
+<header class="sticky top-0 z-50 w-full border-b border-border/70 bg-inherit">
 	<div class="container flex h-16 items-center">
 		<MainNav />
 	</div>
