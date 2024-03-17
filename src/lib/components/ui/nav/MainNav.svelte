9380c05b88a8847b5e31c9718b796c9a9786d6c1
mvkenhov <michiel.vankenhove@ugent.be>
2024-03-17 03:42:22 +0100

    feat: remove results route

--- src/lib/components/ui/nav/MainNav.svelte
+++ src/lib/components/ui/nav/MainNav.svelte
@@ -7,20 +7,16 @@
 	$: userClientAvailable = $userClientStore.client_jwt_token != null;
 </script>
 
-<nav class="w-full flex justify-between">
+<nav class="flex w-full justify-between">
 	<div class="flex items-center space-x-4 lg:space-x-6">
 		<NavLink linkName="Home" href="{base}/" disabled={false} />
-
 		<NavLink linkName="Data" href="{base}/data" disabled={!userClientAvailable} />
-
 		<NavLink linkName="Analysis" href="{base}/analysis" disabled={!userClientAvailable} />
-
-		<NavLink linkName="Results" href="{base}/results" disabled={!userClientAvailable} />
 	</div>
 	<div class="flex items-center space-x-4">
 		<a
 			href="{base}/auth/signout"
-			class="text-sm font-medium transition-colors text-muted-foreground hover:text-primary"
+			class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
 			>Sign out</a
 		>
 		<LightSwitch />
